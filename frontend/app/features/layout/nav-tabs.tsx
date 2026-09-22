c3d46d59d5f97668bb7a4ae730a810113eef5e86
Philip Che Bui <philipcb@stud.ntnu.no>
2026-09-22 21:08:35 +0200

    fix: prevent mobile nav overflow

--- frontend/app/features/layout/nav-tabs.tsx
+++ frontend/app/features/layout/nav-tabs.tsx
@@ -18,7 +18,10 @@ export function NavTabs() {
 
 	const routes = getLinks(t, user?.role ?? null);
 
-	const { setNavLinkRef, pillWidth, pillLeft } = useTabPill(routes, location.pathname);
+	const { setNavLinkRef, pillWidth, pillLeft } = useTabPill(
+		routes,
+		location.pathname,
+	);
 
 	return (
 		<div className="relative mx-auto flex h-11 flex-row rounded-full bg-accent px-2 dark:bg-card">
@@ -32,8 +35,8 @@ export function NavTabs() {
 			{routes.map((route, i) => {
 				const className = ({ isActive }: { isActive: boolean }) =>
 					cn(
-						"z-20 flex cursor-pointer select-none items-center rounded-full px-5 py-2",
-						"text-center font-medium text-muted-foreground text-sm hover:text-foreground",
+						"z-20 flex cursor-pointer select-none items-center rounded-full px-2 py-2 lg:px-5",
+						"text-center font-medium text-muted-foreground text-xs lg:text-sm hover:text-foreground",
 						isActive && "text-foreground",
 					);
 
@@ -49,7 +52,9 @@ export function NavTabs() {
 						className={className}
 						prefetch="intent"
 					>
-						<span className="inline-flex items-center gap-2.5">{route.label}</span>
+						<span className="inline-flex items-center gap-2.5">
+							{route.label}
+						</span>
 					</NavLink>
 				);
 			})}
